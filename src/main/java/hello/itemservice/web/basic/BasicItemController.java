20b633b2ae6e6f5749b0d210c5804ab4f4f517b2
carus-lee <leec8.2019@gmail.com>
2022-04-16 05:06:27 +0900

    # 상품등록 및 처리 - 상품등록 처리시 ModelAttribute로 변천사를 메소드로 생성  > saveV1 ~ V4 메소드

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,15 +7,17 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 
 @Controller
 @RequestMapping("/basic/items")
-//@RequiredArgsConstructor
 public class BasicItemController {
 
 	private final ItemRepository itemRepository;
@@ -42,8 +44,59 @@ public class BasicItemController {
 		return "basic/item";
 	}
 	
+	@GetMapping("/add")
+	public String addForm()
+	{
+		return "basic/addForm";
+	}
+	
+//	@PostMapping("/add")
+	public String saveV1(@RequestParam String itemName,
+						@RequestParam int price,
+						@RequestParam int quantity,
+						Model model)
+	{
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+		
+		itemRepository.save(item);
+		
+		model.addAttribute("item", item);
+
+		return "basic/item";
+	}
+
+//	@PostMapping("/add")
+	public String saveV2(@ModelAttribute("item") Item item)
+	{
+		itemRepository.save(item);
+		// @ModelAttribute로 받을때 "item"을 명시하여 Model에 item 개체가 "자동추가"
+		// 따라서, 아래 라인이 필요없게 됨.
+		// model.addAttribute("item", item); 
+		
+		return "basic/item";
+	}
+	
+//	@PostMapping("/add")
+	public String saveV3(@ModelAttribute Item item)
+	{
+		itemRepository.save(item);
+//		model.addAttribute("item", item); // Item -> item으로 Model에 자동추가됨.
+		
+		return "basic/item";
+	}
+	
+	@PostMapping("/add")
+	public String saveV4(Item item)
+	{
+		itemRepository.save(item);
+		return "basic/item";
+	}
+	
 	@GetMapping("{itemId}/edit")
-	public String itemModify(@PathVariable Long itemId, Item updateParam, Model model)
+	public String edit(@PathVariable Long itemId, Item updateParam, Model model)
 	{
 		itemRepository.update(itemId, updateParam);
 		Item getItem = itemRepository.findById(itemId);
