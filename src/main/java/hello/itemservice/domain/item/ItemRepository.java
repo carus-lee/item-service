48b4f8bc9203f57ded092d9f7b5457c444be6828
carus-lee <leec8.2019@gmail.com>
2022-06-07 12:38:29 +0900

    # 상품 등록/수정/상세 - 타임리프에서 제공하는 태그(th:field 등) 활용방법   > selectBox

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -41,6 +41,7 @@ public class ItemRepository {
 		findItem.setOpen(updateParam.getOpen());
 		findItem.setRegions(updateParam.getRegions());
 		findItem.setItemType(updateParam.getItemType());
+		findItem.setDeliveryCode(updateParam.getDeliveryCode());
 	}
 	
 	public void clearStore()
--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -14,4 +14,7 @@ public enum ItemType
 		this.description = description;
 	}
 	
+	public String getDescription() {
+		return description;
+	}
 }
